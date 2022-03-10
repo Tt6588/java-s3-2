dea608afc6b2104a709abf14c415356cf3ad1860
Tt6588 <tatsusato1011@gmail.com>
2022-03-10 09:58:59 +0900

    [update]Dmm java

---
+++ Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "sato";
+		System.out.println(name);
+		name = "張りゃ";
+		System.out.println(name);
+		int x = 1999;
+		System.out.println(x);
+		
+	}
+}
